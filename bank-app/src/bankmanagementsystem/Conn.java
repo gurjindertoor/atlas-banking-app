4180a5ca4c473a49f1aa41530fd781ffc1c308a8
gurjindertoor <toortalk@gmail.com>
2023-04-25 22:00:51 -0500

    Conn change

--- bank-app/src/bankmanagementsystem/Conn.java
+++ bank-app/src/bankmanagementsystem/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "Koda031402@");
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "root");
             s = c.createStatement();
 
         } catch (Exception e){
